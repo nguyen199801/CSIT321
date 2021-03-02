95f4b536d38df50b1abd0505d5d2ab17842f3ffd
nguyen199801 <nguyennc710@gmail.com>
2021-03-02 22:08:57 +0700

    Added title screen to minigame

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public void Play()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
